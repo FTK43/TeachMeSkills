4090cdfd32ee7857c1c19a909a8e6cebc1c8c04e
Inna Khilko <innakhilko581@gamil.com>
2025-04-14 14:36:58 +0300

    feat: add user crud

--- nest-project/src/modules/users/dtos/user.dto.ts
+++ nest-project/src/modules/users/dtos/user.dto.ts
@@ -1,10 +1,22 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UserDto {
+export class CreateUserDto {
   @IsString()
   @MinLength(3)
   username: string;
 
   @IsEmail()
+  @IsNotEmpty()
   email: string;
+}
+
+export class UpdateUserDto extends CreateUserDto {
+  id: number;
+}
+
+export class UpdateUserPropertiesDto extends PartialType(CreateUserDto) {}
+
+export class UpdatedUserPropertiesDto extends UpdateUserPropertiesDto {
+  id: number;
 }
--- nest-project/src/modules/users/users.service.ts
+++ nest-project/src/modules/users/users.service.ts
@@ -1,13 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { UserDto } from './dtos/user.dto';
-
-export interface IUser extends UserDto {
-  id: number;
-}
+import {Injectable, NotFoundException, OnModuleInit} from '@nestjs/common';
+import {CreateUserDto, UpdatedUserPropertiesDto, UpdateUserDto} from './dtos/user.dto';
 
 @Injectable()
 export class UsersService implements OnModuleInit {
-  private users: IUser[] = [];
+  private users: UpdateUserDto[] = [];
   private currentId: number = 1;
 
   onModuleInit() {
@@ -18,13 +14,55 @@ export class UsersService implements OnModuleInit {
     return this.users;
   }
 
-  createUser(userData: UserDto) {
+  findUserById(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User is not found');
+    }
+    return user;
+  }
+
+  createUser(userData: CreateUserDto) {
     const newUser = {
       id: this.currentId,
       ...userData,
     };
     this.users.push(newUser);
     this.currentId++;
-    return newUser;
+    return userData;
+  }
+
+  updateUser(data: UpdateUserDto) {
+    const updatedUserIndex = this.users.findIndex((user) => user.id === data.id);
+    if (updatedUserIndex < 0) {
+      throw new NotFoundException('User not found');
+    }
+    const updatedUser = {
+      ...this.users[updatedUserIndex],
+      ...data,
+    };
+    this.users.splice(updatedUserIndex, 1, updatedUser);
+    return updatedUser;
+  }
+
+  updateUserProperties(data: UpdatedUserPropertiesDto): UpdatedUserPropertiesDto {
+    const updatedUserIndex = this.users.findIndex((user) => user.id === data.id);
+    if (updatedUserIndex < 0) {
+      throw new NotFoundException('User not found');
+    }
+    const updatedUser = {
+      ...this.users[updatedUserIndex],
+      ...data,
+    };
+    this.users.splice(updatedUserIndex, 1, updatedUser);
+    return updatedUser;
+  }
+
+  deleteUser(id: number) {
+    const deletedUserIndex = this.users.findIndex((user)=> user.id === id);
+    if (deletedUserIndex < 0) {
+      throw new NotFoundException('User not found');
+    }
+    this.users.splice(deletedUserIndex, 1);
   }
 }
